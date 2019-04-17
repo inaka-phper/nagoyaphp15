cf3a87c8459053bd93205a6c78e0317fd0a476c1
phper <inaka.phper@gmail.com>
2019-04-17 20:09:36 +0900

    愚直に解いた

--- src/Dokaku15.php
+++ src/Dokaku15.php
@@ -8,7 +8,32 @@ class Dokaku15
 {
     public function run(string $input): string
     {
-        // implement me
-        return '3445';
+        $input = (int)$input;
+
+        $lines = [];
+        for ($i = 7; $i >= 0; $i--) {
+            $root = (2 ** $i);
+            if ($root <= $input) {
+                $input -= $root;
+                $lines[] = $i;
+            }
+        }
+
+        $result = [];
+        foreach ($lines as $key => $line) {
+            if (array_key_exists($key + 1, $lines)) {
+                $result[] = $line - $lines[$key + 1];
+            } else {
+                $result[] = 8 - $lines[0] + $line;
+            }
+        }
+
+        $triangle = (array_map(function ($side) {
+            return $side === 4 ? $side + 1 : $side + 2;
+        }, $result));
+
+        sort($triangle);
+
+        return join($triangle);
     }
 }
